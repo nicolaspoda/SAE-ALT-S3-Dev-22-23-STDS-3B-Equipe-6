4759ed39fb9d82af35122c456d76b5bda7edef28
OlivierRecher <99925765+OlivierRecher@users.noreply.github.com>
2023-01-18 17:17:57 +0100

    Add files via upload

---
+++ Sprint1/codeV2/src/front.js
@@ -0,0 +1,34 @@
+const socket = io("http://localhost:3001");
+  
+socket.on(encodeURI("STDS/2/Température/T1"), (arg) => {
+  console.log(arg);
+  document.getElementById('tempFut').innerHTML = arg;
+});
+
+  socket.on(encodeURI("STDS/2/Température/T2"), (arg) => {
+    console.log(arg);
+    document.getElementById('tempAmbiante').innerHTML = arg;
+});
+
+  socket.on("STDS/2/Niveau", (arg) => {
+    console.log(arg);
+    document.getElementById('quantite').innerHTML = arg;
+
+  });
+
+  socket.on("STDS/2/Puissance", (arg) => {
+    console.log(arg);
+    document.getElementById('puissance').innerHTML = arg;
+  });
+
+
+  socket.on("STDS/2/Diag", (arg) => {
+    console.log(arg);
+    document.getElementById('diag').innerHTML = arg;
+  });
+
+  // STDS/2/Température/T1
+  // STDS/2/Température/T2
+  // STDS/2/Puissance
+  // STDS/2/Niveau
+  // STDS/2/CO2
